b79ba1e709cbbe85d8abc05c7f09c68768de54a9
MESUT KOÇAKER <mesut0723@gmail.com>
2018-12-15 01:06:56 +0300

    Sıralama Algoritmalarından insertion short
    Insertion Sort Algoritmasının görselleştirilmesi ile ilgili basit bir örnek

---
+++ Sorting_Algorithms_MSTKCKR/Sorting_Algorithms_MSTKCKR/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Sorting_Algorithms_MSTKCKR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 374);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(526, 38);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Trajan Pro", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(11, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Distribute Random";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Trajan Pro", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(171, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 50);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Shorting";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(526, 73);
+            this.panel2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Viner Hand ITC", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(331, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 52);
+            this.label1.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 73);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(526, 301);
+            this.panel3.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 412);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(542, 451);
+            this.MinimumSize = new System.Drawing.Size(542, 451);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Insertion Sort - Mesut Koçaker";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Sorting_Algorithms_MSTKCKR/Sorting_Algorithms_MSTKCKR/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sorting_Algorithms_MSTKCKR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        PictureBox[] pic;
+
+        Random rand = new Random();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = panel3.Controls.Count;
+            for (int i = 0; i < num; i++)
+            {
+                panel3.Controls.RemoveAt(0);
+            }
+            num = 255;
+            pic = new PictureBox[num];
+            ArrayList heightList = new ArrayList();
+            int width = this.Width / 255;
+            for (int i = 0; i < num; i++)
+            {
+                heightList.Add(i + 1);
+                pic[i] = new PictureBox();
+                pic[i].Width = width;
+                pic[i].TabStop = false;
+                pic[i].Height = 5;
+                pic[i].Top = 0;
+                pic[i].Left = i * width + 10;
+                pic[i].Text = "";
+                panel3.Controls.Add(pic[i]);
+                if ((i + 1) % 20 == 0)
+                    panel3.Refresh();
+            }
+            for (int i = 0; i < num; i++)
+            {
+                int r = rand.Next(heightList.Count);
+                pic[i].Height = int.Parse(heightList[r].ToString());
+                pic[i].BackColor = Color.FromArgb(pic[i].Height, 0, 0);
+                heightList.RemoveAt(r);
+                pic[i].Left = i * width + 10;
+                pic[i].Top = panel1.Top - pic[i].Height - 75;
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        int x = 0, y = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            #region
+            if (x < pic.Length)
+            {
+                y = 0;
+                while (y < pic.Length - 1)
+                {
+                    if (pic[x].Height < pic[y].Height)
+                    {
+                        int takas = pic[x].Left;
+                        pic[x].Left = pic[y].Left;
+                        pic[y].Left = takas;
+
+                        PictureBox tak = pic[x];
+                        pic[x] = pic[y];
+                        pic[y] = tak;
+                    }
+                    y++;
+                }
+                x++;
+            }
+            else
+            {
+                label1.Text = " Finish";
+                x = y = 0;
+                timer1.Stop();
+            }
+            #endregion
+            this.MinimumSize = new Size(542, 451);
+        }
+    }
+}
